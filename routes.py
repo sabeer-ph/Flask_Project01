973a4663f30d8b98142dd4723fe9f7ea487561f0
sabeer-ph <shabz.sjec@gmail.com>
2022-10-09 00:30:06 +0400

    edit page done

--- routes.py
+++ routes.py
@@ -4,6 +4,7 @@ Created on Sat Oct  8 13:25:49 2022
 
 @author: husssabe
 """
+#from crypt import methods
 from attr import validate
 from flask import render_template, redirect,url_for, flash, get_flashed_messages
 from App import app, db
@@ -31,3 +32,19 @@ def add():
         flash('Task added to the Db')
         return redirect(url_for('index'))
     return render_template('add.html',form=form)
+
+@app.route('/edit/<int:task_id>',methods=['GET','POST'])
+def edit(task_id):
+    task = Task.query.get(task_id)
+    form = forms.AddTaskForm()
+    if task:
+        if form.validate_on_submit():
+            task.title = form.title.data
+            task.date = dt.utcnow()
+            db.session.commit()
+            flash('Data has been update')
+            return redirect(url_for('index'))
+        form.title.data = task.title
+        return render_template('edit.html',form=form,task_id=task_id)
+
+    return redirect(url_for('index')) 
